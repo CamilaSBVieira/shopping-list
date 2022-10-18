0a65c5040e6863da6d87fa691fbc7c851b511e96
CamilaSBVieira <camilag2.c@gmail.com>
2022-10-18 19:00:41 -0300

    Fix input for iOS

--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -12,7 +12,7 @@ export default function NewItem({ item, handleChange, handleSubmit }) {
                 value={item.name || ''}
                 onChange={handleChange}
                 placeholder='Digite um item...'
-                className='bg-white ring-2 ring-blue-400 rounded-l text-blue-500 p-3 focus:ring-blue-300 outline-0' />
+                className='bg-white ring-2 appearance-none ring-blue-400 rounded-l text-blue-500 p-3 focus:ring-blue-300 outline-0' />
             <button
                 className='bg-blue-400 hover:bg-blue-300 rounded-r text-white hover:ring-blue-300 p-3 ring-2 ring-blue-400'>
                 Add
